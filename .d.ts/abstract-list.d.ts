a1cd78e2ad0c3c16ea8abdef89e920f217b13e5d
T99 <trevorsears.main@gmail.com>
2019-10-02 01:44:08 -0400

    Previous commit missed a handful of compiled files.

--- .d.ts/abstract-list.d.ts
+++ .d.ts/abstract-list.d.ts
@@ -2,7 +2,7 @@ import { IIterator } from "iter-over";
 export declare abstract class AbstractList<E> {
     abstract add(element: E): void;
     addAll(elements: E[]): void;
-    abstract get(index: number): E;
+    abstract get(index: number): E | undefined;
     abstract remove(element: E): void;
     abstract size(): number;
     abstract contains(element: E): boolean;
--- js/abstract-list.js
+++ js/abstract-list.js
@@ -1,1 +1,2 @@
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});class AbstractList{addAll(t){for(let s of t)this.add(s)}isEmpty(){return 0===this.size()}}exports.AbstractList=AbstractList;
+//# sourceMappingURL=abstract-list.js.map
